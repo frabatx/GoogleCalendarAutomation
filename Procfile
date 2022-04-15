6bd1c0030698bfca124c52526b82c267a00d9e95
frabatx <francesco92battista@gmail.com>
2022-04-15 17:30:19 +0200

    procfile modified workers

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn -w 4 -k uvicorn.workers.UvicornWorker cusapi:app
+web: gunicorn cusapi:app
--- routes/calendar.py
+++ routes/calendar.py
@@ -4,23 +4,23 @@ from utils.calendar import create_event, delete_event, get_event, update_event
 routes_calendar = APIRouter()
 
 @routes_calendar.get("/")
-def index(eventId: str):
+def index():
     return {"Hello": "friend!"}
 
-@routes_calendar.post("/create")
-async def create(request: Request):
-    template = await request.json()
-    return create_event(template)
+# @routes_calendar.post("/create")
+# async def create(request: Request):
+#     template = await request.json()
+#     return create_event(template)
 
-@routes_calendar.get("/event/{eventId}")
-def get(eventId: str):
-    return get_event(eventId)
+# @routes_calendar.get("/event/{eventId}")
+# def get(eventId: str):
+#     return get_event(eventId)
 
-@routes_calendar.delete("/delete/{eventId}")
-def delete(eventId: str):
-    return delete_event(eventId)
+# @routes_calendar.delete("/delete/{eventId}")
+# def delete(eventId: str):
+#     return delete_event(eventId)
 
-@routes_calendar.put("/update/{eventId}")
-async def create(request: Request, eventId: str):
-    template = await request.json()
-    return update_event(template, eventId)
+# @routes_calendar.put("/update/{eventId}")
+# async def create(request: Request, eventId: str):
+#     template = await request.json()
+#     return update_event(template, eventId)
