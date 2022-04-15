ff3a1d159f74551240dbf43fac4bc2c1a5cb19ab
frabatx <francesco92battista@gmail.com>
2022-04-15 16:03:00 +0200

    fix method not allowed

--- utils/get_service.py
+++ utils/get_service.py
@@ -11,20 +11,26 @@ SCOPES = [
 
 def get_credentials_google():
     flow = InstalledAppFlow.from_client_secrets_file("google-credentials.json", SCOPES)
+
     creds = flow.run_local_server(port = 80)
 
-    pickle.dump(creds, open('tocken.txt', 'wb'))
+    pickle.dump(creds, open('token.txt', 'wb'))
     return creds
 
 def get_calendar_service():
     creds = None
-    if path.exists('tocken.txt'):
-        creds = pickle.load(open('tocken.txt', 'rb'))
+    if path.exists('token.txt'):
+        creds = pickle.load(open('token.txt', 'rb'))
+        print("token exist")
     # If expired, refresh credentials
     if not creds or not creds.valid:
+        print('credentials not valid')
         if creds and creds.expired and creds.refresh_token:
+            print(f"are_credsexpired? {creds.expired}")
             creds.refresh(Request())
+            print("refresh creds")
         else:
             creds = get_credentials_google()
+            print(f'get_credential because not valid: {creds}')
     service = build('calendar', 'v3', credentials = creds)
     return service
